4d0d72a32be3e1bfe95a962aeb0c504e5cb8ab9c
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2021-04-06 12:04:19 -0300

    feat: create query get landing page

---
+++ src/graphql/queries/getLandingPage.ts
@@ -0,0 +1,12 @@
+///* GraphQL */ Permitir que tenha uma sintaxe do GraphQL que o editor consiga entender
+const GET_LANDING_PAGE = /* GraphQL */ `
+  query GET_LANDING_PAGE {
+    landingPage {
+      logo {
+        alternativeText
+        url
+      }
+    }
+  }
+`
+export default GET_LANDING_PAGE
